07e831785f306ee843bd9c37883e306eac86c118
xieve <2876434-xieve@users.noreply.gitlab.com>
2025-11-05 14:52:03 +0100

    dev: modified docker stuff for my setup

--- scripts/docker/start_arm_container.sh
+++ scripts/docker/start_arm_container.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
-docker run -d \
+#!/usr/bin/env bash
+modprobe -a sg sr_mod vhba
+docker rmi arm
+docker build . -t arm
+docker run \
     -p "8080:8080" \
-    -e ARM_UID="<id -u arm>" \
-    -e ARM_GID="<id -g arm>" \
-    -e TZ="<timedatectl show -p Timezone --value>" \
-    -v "<path_to_arm_user_home_folder>:/home/arm" \
-    -v "<path_to_music_folder>:/home/arm/music" \
-    -v "<path_to_logs_folder>:/home/arm/logs" \
-    -v "<path_to_media_folder>:/home/arm/media" \
-    -v "<path_to_config_folder>:/etc/arm/config" \
-    --device="/dev/sr0:/dev/sr0" \
-    --device="/dev/sr1:/dev/sr1" \
-    --device="/dev/sr2:/dev/sr2" \
-    --device="/dev/sr3:/dev/sr3" \
+    -e ARM_UID="1000" \
+    -e ARM_GID="100" \
+    -e TZ="Europe/Berlin" \
+    -v "$(pwd)/.run:/home/arm" \
+    -v "$(pwd)/.run/music:/home/arm/music" \
+    -v "$(pwd)/.run/logs:/home/arm/logs" \
+    -v "$(pwd)/.run/media:/home/arm/media" \
+    -v "$(pwd)/.run/config:/etc/arm/config" \
+    --device="/dev/sr0:/dev/sr0:rw" \
+    --device="/dev/sg0:/dev/sg0:rw" \
     --privileged \
-    --restart "always" \
-    --name "arm-rippers" \
-    --cpuset-cpus='2,3,4,5,6,7...' \
-    IMAGE_NAME
+    --name=arm \
+    --rm \
+    arm
